d392a1cdccdf4866662f1593a02555608523cc63
jaleu <grey56444@gmail.com>
2019-10-07 11:01:04 -0400

    yes

--- src/index.js
+++ src/index.js
@@ -2,16 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+class Square extends React.Component {
+  render() {
+    return (
+      <button className="square">
+        {this.props.value}
+      </button>
+    )
+  }
+}
+
 class Board extends React.Component {
   renderSquare(i) {
     return <Square value = {i} />;
   }
-}
 
-class Square extends React.Component {
-  render() {
-    <button className = "square">
-      {this.props.value}
-    </button>
-  }
+  render(
+    <div>
+      <div className = "status">{status}</div>
+      <div className = "board-row">
+        {this.renderSquare(0)}
+        {this.renderSquare(1)}
+        {this.renderSquare(2)}
+    </div>
+    <div className="board-row">
+
+  )
 }
